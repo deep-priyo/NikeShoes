b6c78aae31eea77bedd7e20a4980ee5d8e5b937c
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-09-28 21:50:26 +0530

    added services

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,7 +1,10 @@
-import React from "react";
-
+import {services} from '../constants';
 const Services = () => {
-  return <div>Services</div>;
+  return (<section className="max-container flex justify-center flex-wrap gap-9 "> 
+    {services.map((service) => (
+      <ServiceCard key={service.label}{...service}/>
+    ))}
+  </section>);
 };
 
 export default Services;
